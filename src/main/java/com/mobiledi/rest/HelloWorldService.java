d120b95d90acf420fd32ee67871b5b1d2f971d43
Nancy Tran <nancy.tran.1321@gmail.com>
2017-07-28 14:57:14 -0600

    AWS exception in upload method

--- src/main/java/com/mobiledi/rest/HelloWorldService.java
+++ src/main/java/com/mobiledi/rest/HelloWorldService.java
@@ -19,8 +19,9 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.sun.jersey.core.header.FormDataContentDisposition;
+import com.sun.jersey.multipart.FormDataParam;
  
-@Path("hello/")
+@Path("aws/")
 public class HelloWorldService {
  
 	@GET
@@ -34,12 +35,12 @@ public class HelloWorldService {
 	
 	@POST
 	@Path("/upload")
-//	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
 	public String uploadFile(
-//			@FormParam("file") InputStream uploadedInputStream,
-//			@FormParam("file") FormDataContentDisposition fileDetail
+			@FormDataParam("file") InputStream uploadedInputStream,
+			@FormDataParam("file") FormDataContentDisposition fileDetail
 			){
-//		uploadFileToS3(uploadedInputStream, fileDetail);
+		uploadFileToS3(uploadedInputStream, fileDetail);
 		return "File uploaded successfully";
 	}
 	
@@ -54,17 +55,14 @@ public class HelloWorldService {
 			s3client.putObject(new PutObjectRequest(bucketName, keyName, file));
 
 		} catch (AmazonServiceException ase) {
-			System.out.println("Caught an AmazonServiceException, which " + "means your request made it "
-					+ "to Amazon S3, but was rejected with an error response" + " for some reason.");
+			System.out.println("Caught an AmazonServiceException");
 			System.out.println("Error Message:    " + ase.getMessage());
 			System.out.println("HTTP Status Code: " + ase.getStatusCode());
 			System.out.println("AWS Error Code:   " + ase.getErrorCode());
 			System.out.println("Error Type:       " + ase.getErrorType());
 			System.out.println("Request ID:       " + ase.getRequestId());
 		} catch (AmazonClientException ace) {
-			System.out.println("Caught an AmazonClientException, which " + "means the client encountered "
-					+ "an internal error while trying to " + "communicate with S3, "
-					+ "such as not being able to access the network.");
+			System.out.println("Caught an AmazonClientException.");
 			System.out.println("Error Message: " + ace.getMessage());
 		}
     }
